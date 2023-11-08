23fda5b07b84b873c113c7f4cbba6e647b85d1e7
EvanVallet <118521328+EvanVallet@users.noreply.github.com>
2023-11-08 16:49:29 +0100

    Update client.py

--- socket/client.py
+++ socket/client.py
@@ -3,7 +3,7 @@ import socket
 
 
 def connection(host,port,i = 0 ):
-    if i>10:
+    if i<4:
         try:
             client_socket = socket.socket()
             client_socket.connect((host, port))
@@ -11,19 +11,23 @@ def connection(host,port,i = 0 ):
         except ConnectionError:
             print("probleme connection serveur")
             return connection(host,port,i+1)
+        else:
+            client_socket.close()
 
     else:
         raise ConnectionRefusedError("connection ferme")
 
 
 if __name__ == "__main__":
-    try:
-        host = "10.128.2.13"
-        port = 10000
-        connection(host,port)
-    except ConnectionRefusedError:
-        print("fin la connection")
-    else:
-        client_socket = socket.socket()
-        message = input("quel message envoyer")
-        client_socket.send(message.encode())
+        try:
+            host = "127.0.0.1"
+            port = 11112
+            connection(host,port)
+        except ConnectionRefusedError as err:
+            print(err)
+        else:
+            client_socket = socket.socket()
+            client_socket.connect((host, port))
+            while True:
+                message = input("quel message envoyer")
+                client_socket.send(message.encode())
