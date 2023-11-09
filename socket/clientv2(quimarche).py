3143693dfbd8ba4968c52ee25b53d393cf7a21df
EvanVallet <118521328+EvanVallet@users.noreply.github.com>
2023-11-09 17:17:14 +0100

    Update clientv2(quimarche).py

--- socket/clientv2(quimarche).py
+++ socket/clientv2(quimarche).py
@@ -1,9 +1,18 @@
 import socket
 
+
+flag = False
 host = "127.0.0.1"
 port = 11112
 client_socket = socket.socket()
 client_socket.connect((host, port))
-while True:
+
+while not flag:
         message = input("quel message envoyer")
         client_socket.send(message.encode())
+        reply =client_socket.recv(1024).decode()
+        print(reply)
+        if  reply == "stop":
+                flag = True
+
+client_socket.close()
