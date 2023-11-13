58481bcff770afa3853456576fdf43b48dfa4874
EvanVallet <118521328+EvanVallet@users.noreply.github.com>
2023-11-13 15:13:52 +0100

    Update server.py

--- socket/server.py
+++ socket/server.py
@@ -2,20 +2,23 @@ import socket
 import threading
 
 flag = False
-port = 11112
+port = 11111
 server_socket = socket.socket()
 server_socket.bind(('0.0.0.0', port))
 server_socket.listen(1)
 
+class ArretError(Exception):
+    pass
 
 
-
-def reception(server_socket):
+def reception():
     flag = False
+    global conn , server_socket , ArretError
     while not flag:
+        print(1)
         try:
             message = conn.recv(1024).decode()
-        except:
+        except :
             conn, address = server_socket.accept()
 
         else:
@@ -28,24 +31,28 @@ def reception(server_socket):
                  reply = "stop"
                  conn.send(reply.encode())
                  flag = True
+                 server_socket.close()
             else :
                 reply="message pris en compte sont :\n         bye = deconexion \n         arret = arret"
                 conn.send(reply.encode())
     server_socket.close()
 
-retour = threading.Thread(target=reception,args=server_socket)
+retour = threading.Thread(target=reception)
 
-while not flag:
-    try:
-        # try:
-        #     message = str(input("message a envoyer"))
-        #     conn.send(message.encode())
-        # except NameError:
-        #     conn, address = server_socket.accept()
+start= False
+try:
+    conn, address = server_socket.accept()
+    while not flag:
+        if start == False :
+            print("start")
+            retour.start()
+            start = True
+        message=str(input("message a envoyer"))
+        conn.send(message.encode())
+except ConnectionAbortedError:
+    print("arret du server")
+    flag = True
 
-    except OSError:
-        print("arret du server")
-        flag = True
 
 
 
